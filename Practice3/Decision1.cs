5058f3cc39b343fd5efc45c47a9788933aee0bf0
Vitaliy Bogatov <x-vitaliy.bogatov@coyote.com>
2021-03-09 23:58:08 +0200

    Homework3 with the second decision has been implemented

---
+++ Practice3/Decision1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice3
+{
+    class Decision1
+    {
+        List<int> _list = new List<int>();
+
+        public int MinRange { get; set; }
+        public int MaxRange { get; set; }
+
+        public void SetRange()
+        {
+            Console.WriteLine("Plase set min and max value that can be added into the array.");
+            Console.WriteLine("Min value:");
+            MinRange = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Max value:");
+            MaxRange = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public void AddValues()
+        {
+            while (true)
+            {
+                Console.WriteLine($"Type a number between {MinRange} and {MaxRange} or click enter to see results");
+                String input = Console.ReadLine();
+
+                if (int.TryParse(input, out int val) == true && (val >= MinRange && val <= MaxRange))
+                {
+                    _list.Add(val);
+                }
+                else if (int.TryParse(input, out int j) && j < MinRange || j > MaxRange)
+                {
+                    Console.WriteLine($"Value {j} is out of range {MinRange} and {MaxRange}");
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+
+        public int GetResult()
+        {
+            int _result = 0;
+
+            foreach (int element in _list)
+            {
+                if ((element % 3) == 0 && (element % 5) != 0)
+                {
+                    _result += element;
+                }
+            }
+            return _result;
+        }
+    }   
+}
---
+++ Practice3/Decision2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice3
+{
+    class Decision2
+    {
+        public int MinRange { get; set; }
+        public int MaxRange { get; set; }
+
+        public int[] SetArrayValues()
+        {
+            int[] _arr = new int[MaxRange - MinRange];
+
+            for (int i = 0; i < _arr.Length-1; i++)
+            {
+                _arr[i] = MinRange;
+                MinRange++;
+            }
+            return _arr;
+        }
+
+        public int GetValuesDividedThreeNotDividedFive(int[] arr)
+        {
+            int _result = 0;
+            for (int i = 0; i<arr.Length-1; i++)
+            {
+                int temp = arr[i];
+                if ((temp % 3) == 0 && (temp % 5) != 0)
+                {
+                    _result += temp;
+                }
+            }
+            return _result;
+        }
+    }
+}
